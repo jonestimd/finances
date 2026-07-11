1d5236b27483f61a186603b09e8c7cbd5307f0ec
Tim Jones <jonestimd@gmail.com>
2026-07-11 11:12:30 -0500

    don't show duplicate errors

--- cxx/ui/widget/dialog.cpp
+++ cxx/ui/widget/dialog.cpp
@@ -2,10 +2,16 @@
 #include <QTimer>
 
 namespace dialog {
+    static QMultiHash<const QWidget*, const QString> windowErrors{};
+
     void showError(QWidget *parent, const QString message) {
-        QTimer::singleShot(0, parent, [parent, message] {
-            QMessageBox::critical(parent, QMessageBox::tr("Error"), message, QMessageBox::Ok);
-        });
+        if (!windowErrors.contains(parent, message)) {
+            windowErrors.insert(parent, message);
+            QTimer::singleShot(0, parent, [parent, message] {
+                QMessageBox::critical(parent, QMessageBox::tr("Error"), message, QMessageBox::Ok);
+                windowErrors.remove(parent, message);
+            });
+        }
     }
 
     bool confirmDelete(QWidget *parent, const QString title, const QString message, QStringList items) {
